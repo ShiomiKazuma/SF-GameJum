8cedd87a93ab6508549d7f980806b8cafb8017ba
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-07-16 15:51:19 +0900

    [fix]プレイヤーのHP管理を修正

--- Assets/Shiomi/ScriptShiomi/PlayerHp.cs
+++ Assets/Shiomi/ScriptShiomi/PlayerHp.cs
@@ -4,31 +4,25 @@ using Slider = UnityEngine.UI.Slider;
 
 public class PlayerHp : MonoBehaviour
 {
+    //HPバーを設定
+    [SerializeField] private Slider _slider = default;
+
     /// <summary>体力の最大値/// </summary>
     [SerializeField] float _maxHp = 100.0f;
     /// <summary>現在の体力</summary>
-    [SerializeField] float _curentHp;
-    Slider _slider;
+    float _curentHp;
     // Start is called before the first frame update
     void Start()
     {
-        //Sliderを取得
-        _slider = GetComponent<Slider>();
         //現在のHPを最大値に初期化
         _curentHp = _maxHp;
     }
     
-    // Update is called once per frame
-    void Update()
-    {
-        //  Hpバーの更新
-        _slider.value = _curentHp / _maxHp;
-    }
-
     //プレイヤーがダメージを受けた時の処理
     public void PlayerDamage(float damage)
     {
         _curentHp -= damage;
-        Debug.Log(_curentHp);
+        //HPバーの更新
+        _slider.value = _curentHp / _maxHp;
     }
 }
